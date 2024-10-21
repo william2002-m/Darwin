834311f8e471f68a1089d80393930bafb647dad7
✰ sᴀᴛʏᴀ ʙᴀʙʏ ✰ <tridevk442@gmail.com>
2024-10-21 14:29:31 +0530

    Update __main__.py

--- RAUSHAN/__main__.py
+++ RAUSHAN/__main__.py
@@ -1,5 +1,15 @@
+from flask import Flask
 from RAUSHAN import LOGGER, AMBOT
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Bot is running"
 
 if __name__ == "__main__":
     LOGGER.info("The PURVI CHAT BOT Started.")
+    # Run Flask server on port 8000
+    app.run(host="0.0.0.0", port=8000)
+    # Start the bot
     AMBOT().run()
