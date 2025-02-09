0dbd5d85d38c080f0db102bc5dcfec40d2f95249
Block2002 <marwin2005op@gmail.com>
2025-02-09 19:54:51 +0530

    Update ping.py

--- RAUSHAN/modules/ping.py
+++ RAUSHAN/modules/ping.py
@@ -30,7 +30,7 @@ async def ping(_, message: Message):
 
     ms = (datetime.now() - start).microseconds / 1000
     await loda.edit_text(
-        text=f"нєყ вαву!!\n{BOT_NAME} 𝚒ѕ al𝚒ve 🥀 αnd worĸɪng ғɪnє wɪтн ᴀ ᴘɪɴɢ oғ\n➥ `{ms}` ms\n\n<b> мα𝙳є ω𝚒тн ❣️ ву [𝗔𝗟𝗣𝗛𝗔 𝗕𝗔𝗕𝗬](https://t.me/{OWNER_USERNAME}) </b>",
+        text=f"нєყ вαву!!\n{BOT_NAME} 𝚒ѕ al𝚒ve 🥀 αnd worĸɪng ғɪnє wɪтн ᴀ ᴘɪɴɢ oғ\n➥ `{ms}` ms\n\n<b> мα𝙳є ω𝚒тн ❣️ ву [𝘾𝙪𝙧𝙨𝙚𝙙 𝘾𝙝𝙞𝙡𝙙 🤍](https://t.me/{OWNER_USERNAME}) </b>",
         reply_markup=InlineKeyboardMarkup(PNG_BTN),
     )
     if message.chat.type == ChatType.PRIVATE:
